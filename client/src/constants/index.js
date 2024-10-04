c0144015c5e26eea9ecbef1633193b9785c51cf8
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-04 19:23:09 +0600

    Constant commit

---
+++ client/src/constants/index.js
@@ -0,0 +1,30 @@
+export const PROJECT_NAME = "IoTraffix";
+
+export const PROJECT_NAME_CAP = "IoTRAFFIX";
+
+export const PROJECT_LAUNCH_YEAR = 2024;
+
+export const HERO_CONTENT = "This is FinanSage, your personal finance manager";
+
+export const NAVBAR_DROPLISTS = [
+  {
+    header : "Gadgets",
+    elements : ["Wallets", "Budgets", "Reports", "Calculators", "Coinage"]
+  },
+  {
+    header : "Accounts",
+    elements : ["Bank", "FinanSage", "Merchant"]
+  },
+  {
+    header : "Transactions",
+    elements : ["Send Money", "Make Payment", "Pay Bill", "Bank Transfers", "Cash Out"]
+  },
+  {
+    header : "Resource Hub",
+    elements : ["Articles", "Insights"]
+  },
+  {
+    header : "Connect",
+    elements : ["FinanCom", "Market Trends", "News Portal"]
+  },
+]
