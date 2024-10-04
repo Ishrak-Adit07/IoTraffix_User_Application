4e8d635ffc5250abc09ccf7a2a8ac8a0a7db6f86
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-04 19:18:08 +0600

    Routes protection files commit

---
+++ client/src/routesProtection/GuestRoutes.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
+
+
+const GuestRoutes = () => {
+
+    const {user} = useContext(UserContext);
+  return (
+      !user.email ? <Outlet /> : <Navigate to="dashboard"/> 
+  );
+}
+
+export default GuestRoutes;
